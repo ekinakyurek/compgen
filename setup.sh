5b2f3a090f0ee6dc07a193544dbacef46d82fbad
ekinakyurek <akyurekekin@gmail.com>
2020-06-10 00:28:33 -0400

    anonymity WIP

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 jlversion=`julia -v | grep -Eo '[0-9]+\.[0-9]+\.[0-9]'`
 if [ "$jlversion" != "1.2.0" ]; then
   echo -n "Your julia version ${jlversion} is not compatible with requirements,
-           do you want me to install another julia with version 1.2.0 ? If yes, give a path, if no write n [path/NO]: "
+           do you want me to install another julia with version 1.2.0 ? If yes, give a path, if no write no [path/no]: "
   read answer
-  if [ "$answer" = "n" ]; then
+  if [ "$answer" = "no" ]; then
     echo "exitting"
     exit 1
   else
     path=$answer
     echo "julia 1.2.0 will be installed to ${path}"
-    # ./install-julia.sh 1.2.0 $path
+    echo -n "Do you want to continue? press any button"
+    read answer
+    ./install-julia.sh 1.2.0 $path
     jlpath=${path}/julia-1.2.0/bin/julia
     echo $jlpath
     if [ -f $jlpath ]; then
@@ -24,15 +26,15 @@ if [ "$jlversion" != "1.2.0" ]; then
 else
   echo "You've correct version of Julia"
 fi
-#julia --project -e 'using Pkg; Pkg.instantiate();'
-#julia --project -L parser.jl -e 'download(SIGDataSet); download(SCANDataSet)'
-#curl --url https://people.csail.mit.edu/akyurek/recomb/jump.jld2 --output data/SCANDataSet/jump.jld2
-#curl --url https://people.csail.mit.edu/akyurek/recomb/around_right.jld2 --output data/SCANDataSet/around_right.jld2
-# python data/prep.py data/SIGDataSet turkish
-# mkdir checkpoints/SIGDataSet/turkish
-# mkdir checkpoints/SIGDataSet/turkish/logs
-# cp checkpoints/SIGDataSet/collect.sh checkpoints/SIGDataSet/turkish/
-# python data/prep.py data/SIGDataSet spanish
-# mkdir checkpoints/SIGDataSet/spanish
-# mkdir checkpoints/SIGDataSet/spanish/logs/
-# cp checkpoints/SIGDataSet/collect.sh checkpoints/SIGDataSet/spanish/
+julia --project -e 'using Pkg; Pkg.instantiate();'
+julia --project -L parser.jl -e 'download(SIGDataSet); download(SCANDataSet)'
+#FIXME: Needs anonymity
+curl --url https://people.csail.mit.edu/akyurek/recomb/jump.jld2 --output data/SCANDataSet/jump.jld2
+curl --url https://people.csail.mit.edu/akyurek/recomb/around_right.jld2 --output data/SCANDataSet/around_right.jld2
+mkdir checkpoints
+mkdir checkpoints/SIGDataSet/
+for lang in turkish spanish swahili; do
+  mkdir checkpoints/SIGDataSet/${lang}
+  mkdir checkpoints/SIGDataSet/${lang}/logs
+  python data/prep.py data/SIGDataSet ${lang}
+done
